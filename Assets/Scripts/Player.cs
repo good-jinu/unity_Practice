7fb5de42717aa97f72b09117b93fa02caa9de2a6
Jinu <gooday6983@gmail.com>
2020-12-22 19:49:12 +0900

    Organize Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,17 +18,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
-
+    }
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         transform.position += moveVec * speed * Time.deltaTime * (wDown ? 0.3f : 1f);
 
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
-
+    }
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
     }
 }
